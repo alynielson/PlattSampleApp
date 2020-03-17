51b14b5d8aee724c8f1385531c09d70ad7a57c88
alynielson <nielsonaly@gmail.com>
2020-03-17 18:42:48 -0500

    Initial commit: create a .net core 2.0 app, pull files from original repo

---
+++ PlattSampleApp/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PlattSampleApp.Models;
+
+namespace PlattSampleApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetAllPlanets()
+        {
+            var model = new AllPlanetsViewModel();
+
+            // TODO: Implement this controller action
+
+            return View(model);
+        }
+
+        public IActionResult GetPlanetTwentyTwo(int planetid)
+        {
+            var model = new SinglePlanetViewModel();
+
+            // TODO: Implement this controller action
+
+            return View(model);
+        }
+
+        public IActionResult GetResidentsOfPlanetNaboo(string planetname)
+        {
+            var model = new PlanetResidentsViewModel();
+
+            // TODO: Implement this controller action
+
+            return View(model);
+        }
+
+        public IActionResult VehicleSummary()
+        {
+            var model = new VehicleSummaryViewModel();
+
+            // TODO: Implement this controller action
+
+            return View(model);
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
